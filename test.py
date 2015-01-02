84524cd142a88b0e45cb499542773ae23ba4617d
Lukas Matt <lukas@zauberstuhl.de>
2015-01-02 17:06:03 +0100

    Test script should use config file
    The script will need a command-line parameter to identify
    the pod you want to test. After that it will use the config file

--- test.py
+++ test.py
@@ -1,75 +1,88 @@
 #! /usr/bin/python
 
 import sys
+import os.path
 import diaspy
+import configparser
 from time import sleep
 from random import randint
 
-sechat = diaspy.connection.Connection(pod='https://sechat.org',
-        username='user',
-        password='s3cret')
+if len(sys.argv) != 2:
+    print(sys.argv[0] + " <pod-you-want-to-test>")
+    sys.exit(1)
 
-zio = diaspy.connection.Connection(pod='https://diasp.org',
-        username='user',
-        password='secr3t')
+if not os.path.isfile("config.ini"):
+    print("Please create the 'config.ini' first!")
+    sys.exit(1)
 
-sechat.login()
-zio.login()
+localPod = sys.argv[1]
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-### pod one init
+for remotePod in config.sections():
+    if remotePod == localPod:
+        continue
 
-sechatAspect = diaspy.models.Aspect(sechat, name='Testing')
-sechatUser = diaspy.people.User(sechat, handle='zio@diasp.org', fetch='data')
+    local = diaspy.connection.Connection(
+            pod = config[localPod]['scheme'] + '://' + localPod,
+            username = config[localPod]['username'],
+            password = config[localPod]['password'])
+    remote = diaspy.connection.Connection(
+            pod = config[remotePod]['scheme'] + '://' + remotePod,
+            username = config[remotePod]['username'],
+            password = config[remotePod]['password'])
 
-try:
-    sechatAspect.addUser(sechatUser['id'])
-except diaspy.errors.AspectError as e:
-    print(e)
-    if '500' in str(e):
-        sys.exit(2)
+    local.login()
+    remote.login()
 
-### pod one end
+    localAspect = diaspy.models.Aspect(local, name='Testing')
+    localUser = diaspy.people.User(local,
+            handle = config[remotePod]['username'] + "@" + remotePod,
+            fetch='data')
+    try:
+        localAspect.addUser(localUser['id'])
+    except diaspy.errors.AspectError as e:
+        print(e)
+        if '500' in str(e):
+            sys.exit(2)
 
+    remoteAspect = diaspy.models.Aspect(remote, name='Testing')
+    remoteUser = diaspy.people.User(remote,
+            handle = config[localPod]['username'] + "@" + localPod,
+            fetch='data')
+    try:
+        remoteAspect.addUser(remoteUser['id'])
+    except diaspy.errors.AspectError as e:
+        print(e)
+        if '500' in str(e):
+            sys.exit(2)
 
+    seed = str(randint(100, 999))
 
-### pod two init
+    remoteStream = diaspy.streams.Stream(remote)
+    remotePost = remoteStream.post(text="ping " + seed, aspect_ids=str(remoteAspect.id))
 
-zioAspect = diaspy.models.Aspect(zio, name='Testing')
-zioUser = diaspy.people.User(zio, handle='sechat@sechat.org', fetch='data')
+    localStream = diaspy.streams.Stream(local)
+    localPost = localStream.post(text="ping " + seed, aspect_ids=str(localAspect.id))
 
-try:
-    zioAspect.addUser(zioUser['id'])
-except diaspy.errors.AspectError as e:
-    print(e)
-    if '500' in str(e):
-        sys.exit(2)
+    sleep(15)
 
-### pod two end
+    remoteStream.update()
+    localStream.update()
 
-seed = str(randint(100, 999))
+    print("Search remoteStream")
+    for post in remoteStream:
+        if "ping " + seed in str(post) and post.author(key='id') == remoteUser['id']:
+            print("Found ping in remoteStream")
+            break
 
-zioStream = diaspy.streams.Stream(zio)
-zioStream.post(text="ping " + seed, aspect_ids=str(zioAspect.id))
+    print("Search localStream")
+    for post in localStream:
+        if "ping " + seed in str(post) and post.author(key='id') == localUser['id']:
+            print("Found ping in localStream")
+            break
 
-sechatStream = diaspy.streams.Stream(sechat)
-sechatStream.post(text="ping " + seed, aspect_ids=str(sechatAspect.id))
-
-sleep(10)
-
-zioStream.update()
-sechatStream.update()
-
-print("Search zioStream")
-for post in zioStream:
-    if "ping " + seed in str(post) and post.author(key='id') == zioUser['id']:
-        print("Found ping in zioStream")
-        break
-
-print("Search sechatStream")
-for post in sechatStream:
-    if "ping " + seed in str(post) and post.author(key='id') == sechatUser['id']:
-        print("Found ping in sechatStream")
-        break
-
-sechat.logout()
-zio.logout()
+    localPost.delete()
+    remotePost.delete()
+    local.logout()
+    remote.logout()
